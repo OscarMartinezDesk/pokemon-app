74ac13315e11a90d41401ff9d27f397e98d87de0
oscar <oscaralfonsocamachomartinez97@gmail.com>
2021-12-18 14:56:43 -0700

    cambios 2

--- src/components/BtnFlotante/BtnFlotante.js
+++ src/components/BtnFlotante/BtnFlotante.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const BtnFlotante = () => {
     return (
         <div className="btn-flotante">
                 <input type="checkbox" id="btn-mas" />
                 <div className="redes">
-                    <a href="https:www.facebook.com" className="icon-fb a" target="blank_"></a>
-                    <a href="https:www.youtube.com" className="icon-yt a" target="blank_"></a>
-                    <a href="https:www.twitter.com" className="icon-tw a" target="blank_"></a>
-                    <a href="https:www.instagram.com" className="icon-ig a" target="blank_"></a>
+                    <Link to="https:www.facebook.com" className="icon-fb a" target="blank_"></Link>
+                    <Link to="https:www.youtube.com" className="icon-yt a" target="blank_"></Link>
+                    <Link to="https:www.twitter.com" className="icon-tw a" target="blank_"></Link>
+                    <Link to="https:www.instagram.com" className="icon-ig a" target="blank_"></Link>
                 </div>
                 <div className="boton-mas">
                     <label htmlFor="btn-mas" className="icon-mas"></label>
